e61020fc80efc1493213f6bd253aa374f06403d4
WilsonVinson <cprewilsonvinson@gmail.com>
2024-11-30 21:20:42 +0800

    main

--- __init__.py
+++ __init__.py
@@ -18,8 +18,8 @@
 # =       Developer: WilsonVinson        = 
 # = =   未经授权严禁修改分发及商业使用   = = 
 
-__title__ = "MoYu Toolkit"
-__description__ = "MoYu Toolkit --- 摸鱼通用模块工具包"
+__title__ = "MoYuToolkit"
+__description__ = "MoYuToolkit --- 摸鱼通用模块工具包"
 
 __version__ = "0.1.0"
 __author__ = "WilsonVinson"
